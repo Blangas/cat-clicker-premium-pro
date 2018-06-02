ebc872f7857df4e1ef40a337347d01bd24dc52b5
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-06-02 14:19:36 +0200

    feat: JS creates cat class

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-const cats = $('.cat');
+class cat {
+  constructor(catName, img, clickCount){
+    this.catName = catName;
+    this.img = img;
+    this.clickCount = clickCount;
+  }
+}
 
-// Separates cat images in separate div and names them
-cats.each(function(i) {
-  catNames = ['Littly', 'Bigby'];
-  $(this).wrap(`<div class="cat-section"></div>`);
-  $(this).before(`<p class="cat-name">${catNames[i]}</p>`)
-});
 
 
-// Click counter event
-let clickCount = 0;
-
-cats.click(function clickCounter() {
-  clickCount +=1;
-  $('.click-count').text(clickCount);
-});
+//*** OLD CODE ***//
+// const cats = $('.cat');
+//
+// // Separates cat imgs in separate div and names them
+// cats.each(function(i) {
+//   catNames = ['Littly', 'Bigby'];
+//   $(this).wrap(`<div class="cat-section"></div>`);
+//   $(this).before(`<p class="cat-name">${catNames[i]}</p>`)
+// });
+//
+//
+// // Click counter event
+// let clickCount = 0;
+//
+// cats.click(function clickCounter() {
+//   clickCount +=1;
+//   $('.click-count').text(clickCount);
+// });
