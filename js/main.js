445b76aacaf4dd3beef08ebfc3cd74d761aa16ed
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-06-02 18:11:24 +0200

    feat: JS creates 5 cats

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 class cat {
-  constructor(catName, img, clickCount){
+  constructor(catName, altText, img){
     this.catName = catName;
+    this.altText = altText;
     this.img = img;
-    this.clickCount = clickCount;
+    this.clickCount = 0;
   }
 }
 
-
+const cat1 = new cat('Littly', 'Little white kitten with interested expresion', 'img/kitty-cat-kitten-pet-45201.jpeg');
+const cat2 = new cat('Bigby', 'Grown-up cat with bored expresion', 'img/animal-sitting-animals-inside.jpg');
+const cat3 = new cat('Mrs Grey', 'Grey cat standing on two legs', 'img/Grey Kitten On Floor.jpeg');
+const cat4 = new cat('Snowzy', 'Sadly exited cat', 'img/pexels-photo-156934.jpeg');
+const cat5 = new cat('Scare-cat', 'Scared cat face', 'img/pexels-photo-399647.jpeg');
+const cats = [cat1, cat2, cat3, cat4, cat5];
 
 //*** OLD CODE ***//
 // const cats = $('.cat');
