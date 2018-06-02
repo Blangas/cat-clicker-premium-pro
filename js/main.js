c8938e183140feff0dd96870e4ce24fe2c099f03
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-06-02 11:33:23 +0200

    feat: JS separates cats in different div

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-let clickCount = 0;
 const cats = $('.cat');
+
+// Separates cat images in separate div and names them
+cats.each(function(i) {
+  catNames = ['Littly', 'Bigby'];
+  $(this).wrap(`<div class="cat-section"></div>`);
+  $(this).before(`<p class="cat-name">${catNames[i]}</p>`)
+});
+
+
+// Click counter event
+let clickCount = 0;
 
 cats.click(function clickCounter() {
   clickCount +=1;
   $('.click-count').text(clickCount);
-});
-
-catNames = ['Littly', 'Bigby'];
-
-cats.each(function(i) {
-  $(this).wrap(function() {
-    return `<label>${catNames[i]}</label>`;
-  });
 });
