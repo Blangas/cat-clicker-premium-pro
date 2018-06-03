90ef2b916f5edddda3548dddf66608c0f5d54f4d
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-06-03 08:50:57 +0200

    feat: change version to premium pro

--- js/main.js
+++ js/main.js
@@ -7,66 +7,60 @@ class cat {
   }
 }
 
-const cat1 = new cat('Littly', 'Little white kitten with interested expresion', 'img/kitty-cat-kitten-pet-45201.jpeg');
-const cat2 = new cat('Bigby', 'Grown-up cat with bored expresion', 'img/animal-sitting-animals-inside.jpg');
-const cat3 = new cat('Mrs Grey', 'Grey cat standing on two legs', 'img/Grey Kitten On Floor.jpeg');
-const cat4 = new cat('Snowzy', 'Sadly exited cat', 'img/pexels-photo-156934.jpeg');
-const cat5 = new cat('Scare-cat', 'Scared cat face', 'img/pexels-photo-399647.jpeg');
-const cats = [cat1, cat2, cat3, cat4, cat5];
 
-// Function to load selected cat
-function loadCat(c) {
-  const container = $('.cat-container');
-
-  container.html('');
-
-  container.append(function() {
-    const disCounter = `<p class="click-count">${c.clickCount}</p>`;
-    const disName = `<p class="cat-name">${c.catName}</p>`;
-    const disImg = `<img src="${c.img}" alt="${c.altText}">`
-    return `${disCounter}, ${disName}, ${disImg}`;
-  });
-
-  // counting and displaying clicks
-  container.find('img').on('click', function() {
-    c.clickCount +=1;
-    $('.click-count').text(c.clickCount);
-  });
-}
-
-// Loop to create cats links
 $(function() {
-  const list = $('.cat-list');
-  let catId = 0;
+  const model = {
+    cats: [
+      new cat('Littly', 'Little white kitten with interested expresion', 'img/kitty-cat-kitten-pet-45201.jpeg'),
+      new cat('Bigby', 'Grown-up cat with bored expresion', 'img/animal-sitting-animals-inside.jpg'),
+      new cat('Mrs Grey', 'Grey cat standing on two legs', 'img/Grey Kitten On Floor.jpeg'),
+      new cat('Snowzy', 'Sadly exited cat', 'img/pexels-photo-156934.jpeg'),
+      new cat('Scare-cat', 'Scared cat face', 'img/pexels-photo-399647.jpeg')
+    ]
+  };
 
-  for (let cat of cats) {
-    list.append(`<li><a href="#" data-id="${catId}">${cat.catName}</a></li>`);
-    catId +=1;
-  }
+  const octopus = {
+    init: function() {
+      view.initList(model.cats);
+    },
+    catSelect: function() {
+      $('.cat-list').on('click', 'a', function() {
+        view.loadCat(model.cats[$(this).data('id')]);
+      });
+    },
+    catClick: function(cat) {
+      $('.cat-container').find('img').on('click', function() {
+        cat.clickCount +=1;
+        $('.click-count').text(cat.clickCount);
+      });
+    }
+  };
 
-  // Adds event listener and passes function argument by HTML data-id attribute
-  list.on('click', 'a', function() {
-    loadCat(cats[$(this).data('id')]);
-  });
+  const view = {
+    initList: function(cats) {
+      let catId = 0;
 
-}());
+      for (let cat of cats) {
+        $('.cat-list').append(`<li><a href="#" data-id="${catId}">${cat.catName}</a></li>`);
+        catId +=1;
+      }
+      octopus.catSelect();
+    },
+    loadCat: function(cat) {
+      const container = $('.cat-container');
 
+      container.html('');
 
-//*** OLD CODE ***//
-// const cats = $('.cat');
-//
-// // Separates cat imgs in separate div and names them
-// cats.each(function(i) {
-//   catNames = ['Littly', 'Bigby'];
-//   $(this).wrap(`<div class="cat-section"></div>`);
-//   $(this).before(`<p class="cat-name">${catNames[i]}</p>`)
-// });
-//
-//
-// // Click counter event
-// let clickCount = 0;
-//
-// cats.click(function clickCounter() {
-//   clickCount +=1;
-//   $('.click-count').text(clickCount);
-// });
+      container.append(function() {
+        const disCounter = `<p class="click-count">${cat.clickCount}</p>`;
+        const disName = `<p class="cat-name">${cat.catName}</p>`;
+        const disImg = `<img src="${cat.img}" alt="${cat.altText}">`
+        return `${disCounter}, ${disName}, ${disImg}`;
+      });
+
+      octopus.catClick(cat);
+    }
+  };
+
+  octopus.init();
+});
